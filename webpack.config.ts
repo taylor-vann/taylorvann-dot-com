45c6739d2d3e260604065b6f070f528dadd4bc86
taylor-vann <brian@localhost.localdomain>
2019-11-04 16:20:52 -0800

    Setup typescript environment with eslint, prettier, and custom webpack in ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,38 @@
-const path = require("path");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+import * as webpack from "webpack";
+import * as HtmlWebPackPlugin from "html-webpack-plugin";
+import * as MiniCSSExtractPlugin from "mini-css-extract-plugin";
+import * as path from "path";
 
 // html loader
-const html_plugin = new HtmlWebPackPlugin({
-  template: "./src/index.html"
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
 });
 
 // create css plugin
-const css_extract_plugin = new MiniCSSExtractPlugin({
-  filename: "[name].css",
-  chunkFilename: "[id].css",
-  ignoreOrder: false // Enable to remove warnings about conflicting order
+const cssExtractPlugin = new MiniCSSExtractPlugin({
+  filename: "styles/[name].css",
+  chunkFilename: "styles/[id].css",
+  ignoreOrder: false, // Enable to remove warnings about conflicting order
 });
 
 // webpack config for a react app in typescript
-const webpack_config = {
+const webpackConfig: webpack.Configuration = {
   mode: "production",
   entry: "./src/app_root.tsx",
+  output: {
+    filename: "js/bundled_typescript.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   devtool: "inline-source-map",
   resolve: {
-    extensions: [".js", ".ts", ".tsx"]
+    extensions: [".js", ".ts", ".tsx"],
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: "ts-loader",
       },
       {
         exclude: /node_modules/,
@@ -37,10 +42,10 @@ const webpack_config = {
           {
             loader: "css-loader",
             options: {
-              modules: true
-            }
-          }
-        ]
+              modules: true,
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
@@ -51,10 +56,10 @@ const webpack_config = {
             options: {
               localIdentName: "[name]__[local]___[hash:base64:5]",
               modules: true,
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
@@ -63,10 +68,10 @@ const webpack_config = {
             loader: "url-loader",
             options: {
               limit: 10240,
-              name: "styles/fonts/[name]__[hash].[ext]"
-            }
-          }
-        ]
+              name: "styles/fonts/[name]__[hash].[ext]",
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jp(e*)g|svg)$/,
@@ -75,18 +80,14 @@ const webpack_config = {
             loader: "url-loader",
             options: {
               limit: 30720,
-              name: "images/[name]__[hash].[ext]"
-            }
-          }
-        ]
-      }
-    ]
+              name: "images/[name]__[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
-  output: {
-    filename: "bundled_typescript.js",
-    path: path.resolve(__dirname, "dist/js")
-  },
-  plugins: [html_plugin, css_extract_plugin]
+  plugins: [htmlPlugin, cssExtractPlugin],
 };
 
-module.exports = webpack_config;
+module.exports = webpackConfig;
