5a64ca4fbf233ac5cc5ed925c1ed526b4735b069
taylor-vann <brian.t.vann@gmail.com>
2019-12-18 17:41:31 -0800

    Query device utils and create observers for changes in devices

--- web_client/src/device_details/device_details.types.ts
+++ web_client/src/device_details/device_details.types.ts
@@ -1,3 +1,5 @@
+// Browser Details
+
 export const FIREFOX = "FIREFOX";
 export const CHROME = "CHROME";
 export const SAFARI = "SAFARI";
@@ -14,3 +16,36 @@ export type BrowsersType =
   | typeof IE
   | typeof OPERA
   | typeof BLINK;
+
+// Media Details
+export type MediaDevicesMapType = {
+  [deviceId: string]: MediaDeviceInfo;
+};
+
+export type MediaDevicesMapByGroupIdType = {
+  [groupId: string]: {
+    [kind: string]: {
+      [deviceId: string]: MediaDeviceInfo;
+    };
+  };
+};
+
+export type MediaDevicesMapByKindType = {
+  [kind: string]: {
+    [deviceId: string]: MediaDeviceInfo;
+  };
+};
+
+export type GetMediaDevicesRawArgsType = {
+  mediaDevicesById: MediaDevicesMapType;
+  mediaDevicesByGroupId: MediaDevicesMapByGroupIdType;
+  mediaDevicesByKind: MediaDevicesMapByKindType;
+};
+
+export type GetMediaDevicesRawCallbackType = (
+  args: GetMediaDevicesRawArgsType,
+) => void;
+
+export type MediaSubscriptionsMapType = {
+  [key: number]: GetMediaDevicesRawCallbackType;
+};
