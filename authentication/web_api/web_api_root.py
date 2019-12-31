a00489ef7164d46f44ed83df1ebd1c9d58546588
taylor-vann <brian.t.vann@gmail.com>
2019-12-30 16:50:01 -0800

    Get a simple wsgi server going

--- authentication/web_api/web_api_root.py
+++ authentication/web_api/web_api_root.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route("/")
-    def hello_world():
-        return jsonify(hello="world"})
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
