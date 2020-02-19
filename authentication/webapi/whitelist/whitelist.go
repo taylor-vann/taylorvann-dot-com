13c66e8917dc15704e8394464ee6849ff86180dc
taylor-vann <brian.t.vann@gmail.com>
2020-02-18 18:53:20 -0800

    test and clean whitelist

--- authentication/webapi/whitelist/whitelist.go
+++ authentication/webapi/whitelist/whitelist.go
@@ -1,35 +1,114 @@
 package whitelist
 
 import (
-	"fmt"
-
+	"encoding/json"
+	"errors"
 	"webapi/interfaces/whitelistx"
+	"webapi/utils"
 )
 
-// HelloWorld - ping
-func HelloWorld() {
-	whitelistx.Get("YODAWG")
-	fmt.Println("bridge the gap")
+// MilliSecond -
+type MilliSecond = int64
+
+// Entry -
+type Entry struct {
+	CsrfToken  []byte             `json:"csrf_token"`
+	SessionKey []byte             `json:"session_key"`
+	CreatedAt  utils.MilliSeconds `json:"created_at"`
+	Lifetime   utils.MilliSeconds `json:"expires_at"`
 }
 
+// CreateEntryParams -
+type CreateEntryParams struct {
+	CreatedAt  utils.MilliSeconds
+	Lifetime   utils.MilliSeconds
+	SessionKey *[]byte
+	Signature  *string
+}
 
-// verify token is in whitelist
-// create token, add to whitelist, and return token
-// remove token from whitelist
+// ReadEntryParams -
+type ReadEntryParams struct {
+	Signature *string
+}
 
-// store for jti, minimal security details
-// jti: token_specific_secret_key
+// DayAsMS -
+var DayAsMS = int64(1000 * 60 * 60 * 24)
 
-// Only required pulic methods
-// Exists return Receipts
-// - called every request
+// ThreeDaysAsMS -
+var ThreeDaysAsMS = 3 * DayAsMS
 
-// Add - can only be called from our password / fingerprint
-// authentication
+// RemoveEntryParams -
+type RemoveEntryParams = ReadEntryParams
 
-// only two services allowed
-// - nginx / initial loadbalancer
-// - security logging (potentially)
+// Entry params add user and audience
+func composeCreatePublicEntryParams() {}
 
-// This is also where you'd log
-// to a secondary service focused on
+// CreateEntry -
+func CreateEntry(p *CreateEntryParams) (*Entry, error) {
+	if p == nil {
+		return nil, errors.New("nil parameters provided")
+	}
+
+	csrfToken, errCsrfToken := utils.GenerateRandomByteArray(128)
+	if errCsrfToken != nil {
+		return nil, errCsrfToken
+	}
+
+	entry := Entry{
+		CsrfToken:  *csrfToken,
+		SessionKey: *(p.SessionKey),
+		CreatedAt:  p.CreatedAt,
+		Lifetime:   p.Lifetime,
+	}
+
+	// marshal entry to byte array
+	entryAsJSON, errEntryAsJSON := json.Marshal(entry)
+	if errEntryAsJSON != nil {
+		return nil, errEntryAsJSON
+	}
+
+	// save to whitelist
+	whitelistResult, errWhitelist := whitelistx.SetAndExpire(
+		*(p.Signature),
+		&entryAsJSON,
+		p.Lifetime,
+	)
+
+	if errWhitelist != nil {
+		return nil, errWhitelist
+	}
+
+	if whitelistResult == true {
+		return &entry, errWhitelist
+	}
+
+	return nil, errCsrfToken
+}
+
+// ReadEntry -
+func ReadEntry(p *ReadEntryParams) (*Entry, error) {
+	if p == nil {
+		return nil, errors.New("nil parameters provided")
+	}
+
+	entryAsByte, errEntryAsByte := whitelistx.Get(*(p.Signature))
+	if errEntryAsByte != nil {
+		return nil, errEntryAsByte
+	}
+
+	var entry Entry
+	errUnmarshal := json.Unmarshal(*entryAsByte, &entry)
+	if errUnmarshal != nil {
+		return nil, errUnmarshal
+	}
+
+	return &entry, errUnmarshal
+}
+
+// RemoveEntry -
+func RemoveEntry(p *RemoveEntryParams) (bool, error) {
+	if p == nil {
+		return false, errors.New("nil parameters provided")
+	}
+	return whitelistx.Remove(*(p.Signature))
+}
