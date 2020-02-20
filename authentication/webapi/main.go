3f5d24567da2ac14c7d980e46bf1c80f1ecbdce3
taylor-vann <brian.t.vann@gmail.com>
2020-02-20 15:48:39 -0800

    pre flight checkin, these main and routes are not ready but the final piece of the puzzle!

--- authentication/webapi/main.go
+++ authentication/webapi/main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"webapi/constants"
 	"webapi/server"
-	// "webapi/store"
-	"webapi/whitelist"
 )
 
 func main() {
-	// create database if necessary
-	// store.CreateRequiredDatabases()
-	constants.SetEnvironmentConstants()
-
-	// ping our redis store
-	whitelist.HelloWorld()
-
-	// create server
 	server.CreateServer(5000)
 }
--- authentication/webapi/routes/routes.go
+++ authentication/webapi/routes/routes.go
@@ -2,28 +2,29 @@ package routes
 
 import (
 	"net/http"
-
-	"webapi/hooks"
+	// "webapi/hooks"
 )
 
 // CreateRoutes - add hooks to route callbacks
 func CreateRoutes(mux *http.ServeMux) *http.ServeMux {
 	// ping - information endpoint about our authentication api
-	mux.HandleFunc("/", hooks.Ping)
+	// mux.HandleFunc("/", hooks.Ping)
 
-	// personal accounts - endpoints regarding personal details
-	mux.HandleFunc("/create_user", hooks.CreateUser)
-	mux.HandleFunc("/update_account_details", hooks.Ping)
-	mux.HandleFunc("/update_password", hooks.Ping)
-	mux.HandleFunc("/remove_user", hooks.Ping)
+	// /store/m
+	// /store/q
+	// check cache for stale request, return stale receipt
+	// set query to pending
+	// query store
+	// save in cache
+	// mux.HandleFunc("/store/", hooks.Ping)
 
-	// validation - arbitrarily validate a user's password
-	mux.HandleFunc("/validate_user", hooks.Ping)
-
-	// sessions - mux.HandleFunc("/create_session", hooks.Ping)
-	mux.HandleFunc("/invalidate_session", hooks.Ping)
-	// our most frequently called method
-	mux.HandleFunc("/return_session", hooks.Ping)
+	// /q
+	// read a cache
+	// query store if nothing in cache
+	// store results in cache
+	// we don't count sessions as mutations because
+	// our sessions are supposed to be stateless
+	// mux.HandleFunc("/sessions/", hooks.Ping)
 
 	return mux
 }
