c94f07b47d8870456aa43cbd9c8d08a70b902871
Brian Taylor Vann <brian.t.vann@gmail.com>
2020-08-13 06:40:04 +0000

    create fileserver for webclients

--- clients/webapi/routes/routes.go
+++ clients/webapi/routes/routes.go
@@ -5,55 +5,16 @@ package routes
 
 import (
 	"net/http"
-	"os"
-	"strings"
 
-	"webapi/routes/ping"
+	"webapi/fileserver"
 )
 
-var (
-	webClientsDirectory = os.Getenv("WEB_CLIENTS_DIRECTORY")
-	
-	directoryRune = []byte("/")[0]
-	relativeRune = []byte(".")[0]
 
-	fileServer = http.FileServer(http.Dir(webClientsDirectory))
-)
-
-func isValidPath(path string) bool {
-	searchIndex := 0
-	pathLength := len(path)
-	for searchIndex < pathLength {
-		searchIndex += 1
-		if path[searchIndex] == relativeRune && path[searchIndex - 1] == directoryRune {
-			return false
-		}
-	}
-
-	return true
-}
-
-// Blog Location
-func serveStaticFiles(w http.ResponseWriter, r *http.Request) {
-	if strings.HasSuffix(r.URL.Path, "/") {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	// check for correct paths
-	if isValidPath(r.URL.Path) {
-		fileServer.ServeHTTP(w, r)
-	}
-
-	// default 404
-	w.WriteHeader(http.StatusNotFound)
-}
 
 func CreateMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/ping", ping.Details)
-	mux.HandleFunc("/", serveStaticFiles)
+	mux.HandleFunc("/", fileserver.Serve)
 
 	return mux
 }
