068f512ef6dc3e32ccaf8412a9e271c199aeae43
Brian Taylor Vann <brian.t.vann@gmail.com>
2020-09-06 20:25:02 +0000

    update(clients): crudly add unit testing in mocha and jasmine

---
+++ clients/webclients/sign-in/development/main.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import "mocha";
+
+describe("Main", () => {
+  it("should fail immediately", () => {
+    expect(false).to.be.equal(true);
+  });
+});
--- clients/webclients/sign-in/sign-in/scripts/bundled.js
+++ clients/webclients/sign-in/sign-in/scripts/bundled.js
@@ -1,2 +1,16 @@
-// const signInForm: HTMLElement | null = document.querySelector("#sign-in-form");
 console.log("HELLO WORLD!");
+const signInForm = document.querySelector("#form");
+const submitButton = document.querySelector("#submit-button");
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (signInForm) {
+        const formData = new FormData(signInForm);
+        const jsonFormData = Object.fromEntries(formData); // get entries from iterable
+        jsonFormData["environment"] = "DEVELOPMENT";
+        console.log(jsonFormData);
+    }
+};
+// add event listener on click
+submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener("click", handleSubmit);
+// on click
+// print form data
